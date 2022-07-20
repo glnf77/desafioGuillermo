80c028f2a6e782ff7e5d478763342c385ff9c0b0
glnf77 <glnfeventos@gmail.com>
2022-07-20 11:56:45 -0300

    Revert "Merge branch 'master' of https://github.com/glnf77/desafioGuillermo"
    This reverts commit b1212c86c094d1113f5d737a64ae2703af15525b, reversing
    changes made to a405f8cab58993d5adff0eca64ef6499644cdd06.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let ticket = 200
 function asignarValor(){
     let nombre = document.getElementById('nombre').value
-    let mail = document.getElementById('email').value
     let cantidad = document.getElementById('cantidadEntrada').value
     cantidad = parseInt(cantidad)
     let categoria = document.getElementById('inputState').value
@@ -9,18 +8,15 @@ function asignarValor(){
     switch (categoria) {
         case 'estudiante':
             let totalEstudiante = (ticket - ((ticket*80)/100)) * cantidad
-            document.getElementById('totalCompra').textContent = [nombre+' el total a pagar, por ser Estudiante es de: $'+totalEstudiante]
-            document.getElementById('totalCompra1').textContent = ['El ticket sera enviado al email: '+mail]
+            document.getElementById('totalCompra').textContent = nombre+' el total a pagar, por ser Estudiante es de: $'+totalEstudiante
             break;
         case 'trainee':
             let totalTrainee = (ticket - ((ticket*50)/100)) * cantidad
-            document.getElementById('totalCompra').textContent = [nombre+' el total a pagar, por ser Trainee es de: $'+totalTrainee]
-            document.getElementById('totalCompra1').textContent = ['El ticket sera enviado al email: '+mail]
+            document.getElementById('totalCompra').textContent = nombre+' el total a pagar, por ser Trainee es de: $'+totalTrainee
             break;
         default:
             let totalJunior = (ticket - ((ticket*15)/100)) * cantidad
-            document.getElementById('totalCompra').textContent = [nombre+' el total a pagar, por ser Junior es de: $'+totalJunior]
-            document.getElementById('totalCompra1').textContent = ['El ticket sera enviado al email: '+mail]
+            document.getElementById('totalCompra').textContent = nombre+' el total a pagar, por ser Junior es de: $'+totalJunior
             break;
     }
 }
